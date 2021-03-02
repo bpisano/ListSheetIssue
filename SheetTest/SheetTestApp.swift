68b6f84d4441c7d30828f06dfe5454b7c43bc4a3
Benjamin Pisano <benjamin.pisano@icloud.com>
2021-03-02 11:46:29 +0100

    Initial Commit

---
+++ SheetTest/SheetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetTestApp.swift
+//  SheetTest
+//
+//  Created by Benjamin Pisano on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SheetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
